e5ed13f4ccfffc3b3111d3d9eab283750fb1cfb0
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-10-10 23:15:43 +0300

    Implement text selection inside TextViewModel (Refactoring required)

--- src/main/kotlin/TextViewModel.kt
+++ src/main/kotlin/TextViewModel.kt
@@ -1,8 +1,16 @@
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.key.*
 import components.text.Cursor
 import components.providers.TextProvider
+import components.shortcuts.ShortcutsRegistry
+import components.text.highlighters.Highlighter
+import components.text.highlighters.HighlighterManager
+import components.text.highlighters.SelectionHighlighter
+import kotlin.math.max
+import kotlin.math.min
 
+@OptIn(ExperimentalComposeUiApi::class)
 class TextViewModel {
     /**
      * Consecutive whitespaces are not rendered if there is no basic letter after them.
@@ -10,6 +18,12 @@ class TextViewModel {
      */
     private val nonBreakingSpaceChar = 0xA0.toChar()
     private val textProvider = TextProvider()
+    private val highlighterManager = HighlighterManager()
+
+    val highlighters: List<Highlighter>
+        get() {
+            return highlighterManager.highlighters
+        }
 
     val text: String
         get() {
@@ -25,32 +39,72 @@ class TextViewModel {
         return !ch.isISOControl() && !ch.isIdentifierIgnorable() && ch.isDefined()
     }
 
-    @OptIn(ExperimentalComposeUiApi::class)
     fun processKeyEvent(event: KeyEvent): Boolean {
+        val shortcut: ShortcutsRegistry.ShortcutCombination? = ShortcutsRegistry.identify(event)
+
+        println("shortcut: $shortcut")
+
         if (event.type == KeyEventType.KeyDown) {
-            when (event.key) {
-                Key.Backspace -> textProvider.backspace()
-                Key.Enter -> textProvider.newline()
-                Key.DirectionUp -> textProvider.changeCursorPositionDirectionUp()
-                Key.DirectionRight -> textProvider.changeCursorPositionDirectionRight()
-                Key.DirectionDown -> textProvider.changeCursorPositionDirectionDown()
-                Key.DirectionLeft -> textProvider.changeCursorPositionDirectionLeft()
-                Key.Spacebar -> textProvider.insert(nonBreakingSpaceChar)
-                Key.Delete -> textProvider.delete()
-                else -> {
-                    val ch: Char = event.utf16CodePoint.toChar()
-                    if (isPrintable(ch)) {
-                        textProvider.insert(ch)
-                    }
-                    else {
-                        println("Provided unsupported character '${ch.code}' is non-printable")
-                    }
+            processPressedEventKey(event, shortcut)
+        }
+        return true
+    }
+
+    private fun processPressedEventKey(event: KeyEvent, shortcut: ShortcutsRegistry.ShortcutCombination?) {
+        val oldCursorOffset = cursor.offset
+
+        when (event.key) {
+            Key.Backspace -> textProvider.backspace()
+            Key.Enter -> textProvider.newline()
+            Key.DirectionUp -> textProvider.changeCursorPositionDirectionUp()
+            Key.DirectionRight -> textProvider.changeCursorPositionDirectionRight()
+            Key.DirectionDown -> textProvider.changeCursorPositionDirectionDown()
+            Key.DirectionLeft -> textProvider.changeCursorPositionDirectionLeft()
+            Key.Spacebar -> textProvider.insert(nonBreakingSpaceChar)
+            Key.Delete -> textProvider.delete()
+            else -> {
+                val ch: Char = event.utf16CodePoint.toChar()
+                if (isPrintable(ch)) {
+                    textProvider.insert(ch)
+                }
+                else {
+                    //println("Provided unsupported character '${ch.code}' is non-printable")
                 }
             }
         }
 
-        return true
+        val newCursorOffset = cursor.offset
+
+        adjustSelectionShortcut(shortcut, event, oldCursorOffset, newCursorOffset)
     }
 
+    private fun adjustSelectionShortcut(
+        shortcut: ShortcutsRegistry.ShortcutCombination?, event: KeyEvent, oldCursorOffset: Int, newCursorOffset: Int) {
 
+        when (shortcut) {
+            ShortcutsRegistry.ShortcutCombination.CTRL_A -> {
+                highlighterManager.dismissSelection()
+                highlighterManager.selection(0, text.length)
+            }
+            ShortcutsRegistry.ShortcutCombination.SHIFT_DIRECTION_UP,
+            ShortcutsRegistry.ShortcutCombination.SHIFT_DIRECTION_RIGHT,
+            ShortcutsRegistry.ShortcutCombination.SHIFT_DIRECTION_DOWN,
+            ShortcutsRegistry.ShortcutCombination.SHIFT_DIRECTION_LEFT -> {
+                if (!highlighterManager.containsSelectionHighlighter()) {
+                    highlighterManager.selection(
+                        min(oldCursorOffset, newCursorOffset),
+                        max(oldCursorOffset, newCursorOffset),
+                    )
+                }
+                else {
+                    highlighterManager.adjustSelection(oldCursorOffset, newCursorOffset)
+                }
+            }
+            else -> {
+                if (!(event.isShiftPressed && (event.key == Key.ShiftLeft || event.key == Key.ShiftRight))) {
+                    highlighterManager.dismissSelection()
+                }
+            }
+        }
+    }
 }
