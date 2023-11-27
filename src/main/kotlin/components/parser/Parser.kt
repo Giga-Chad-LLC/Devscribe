03987433593b99bc902e65ee1cc7a814c5d6ee52
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-11-27 11:48:56 +0200

    Add TODO comment into Parser

--- src/main/kotlin/components/parser/Parser.kt
+++ src/main/kotlin/components/parser/Parser.kt
@@ -3,6 +3,7 @@ package components.parser
 import components.lexer.Token
 import components.parser.nodes.AstNode
 
+// TODO: parse() should return an array of encountered errors while parsing
 interface Parser {
     fun parse(tokens: List<Token>): AstNode
 }
